6ec36fd2184b50bcef7638484dd2cd3cc59323c0
Buddhadebroy <roybuddhadeb2003@gmail.com>
2025-03-18 18:03:23 +0530

    update

--- build/blocks/enquiry-button/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd1dd34a58c792714ba9c');
--- build/blocks/setupWizard/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '78175c44ffcbc2a8a129');
